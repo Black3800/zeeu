0636624086abaabe0808d956a2d21abcc6bd67b7
anakint <anakin.t@mail.kmutt.ac.th>
2022-06-15 13:29:33 +0700

    Edit palette

--- lib/utils/palette.dart
+++ lib/utils/palette.dart
@@ -2,121 +2,124 @@ import 'package:flutter/material.dart';
 
 class Palette {
   static const MaterialColor jet = MaterialColor(0xff353535, {
-    50: Color.fromRGBO(53, 53, 53, .1),
+    50: Color.fromRGBO(53, 53, 53, .05),
     100: Color.fromRGBO(53, 53, 53, .2),
-    200: Color.fromRGBO(53, 53, 53, .3),
-    300: Color.fromRGBO(53, 53, 53, .4),
-    400: Color.fromRGBO(53, 53, 53, .5),
-    500: Color.fromRGBO(53, 53, 53, .6),
-    600: Color.fromRGBO(53, 53, 53, .7),
-    700: Color.fromRGBO(53, 53, 53, .8),
-    800: Color.fromRGBO(53, 53, 53, .9),
-    900: Color.fromRGBO(53, 53, 53, 1),
+    200: Color.fromRGBO(53, 53, 53, .4),
+    300: Color.fromRGBO(53, 53, 53, .6),
+    400: Color.fromRGBO(53, 53, 53, .8),
+    500: Color.fromRGBO(53, 53, 53, 1)
   });
 
   static const MaterialColor gold = MaterialColor(0xfff7cb8d, {
-    50: Color.fromRGBO(247, 203, 141, .1),
-    100: Color.fromRGBO(247, 203, 141, .2),
-    200: Color.fromRGBO(247, 203, 141, .3),
-    300: Color.fromRGBO(247, 203, 141, .4),
-    400: Color.fromRGBO(247, 203, 141, .5),
-    500: Color.fromRGBO(247, 203, 141, .6),
-    600: Color.fromRGBO(247, 203, 141, .7),
-    700: Color.fromRGBO(247, 203, 141, .8),
-    800: Color.fromRGBO(247, 203, 141, .9),
-    900: Color.fromRGBO(247, 203, 141, 1),
+    50: Color(0xfffefaf4),
+    100: Color(0xfffdf5e8),
+    200: Color(0xfffcead1),
+    300: Color(0xfffae0bb),
+    400: Color(0xfff9d5a4),
+    500: Color(0xfff7cb8d),
+    600: Color(0xffc6a271),
+    700: Color(0xff947a55),
+    800: Color(0xff635138),
+    900: Color(0xff31291c)
   });
 
   static const MaterialColor honeydew = MaterialColor(0xffe9fff5, {
-    50: Color.fromRGBO(233, 255, 245, .1),
-    100: Color.fromRGBO(233, 255, 245, .2),
-    200: Color.fromRGBO(233, 255, 245, .3),
-    300: Color.fromRGBO(233, 255, 245, .4),
-    400: Color.fromRGBO(233, 255, 245, .5),
-    500: Color.fromRGBO(233, 255, 245, .6),
-    600: Color.fromRGBO(233, 255, 245, .7),
-    700: Color.fromRGBO(233, 255, 245, .8),
-    800: Color.fromRGBO(233, 255, 245, .9),
-    900: Color.fromRGBO(233, 255, 245, 1),
+    50: Color(0xfffdfffe),
+    100: Color(0xfffbfffd),
+    200: Color(0xfff6fffb),
+    300: Color(0xfff2fff9),
+    400: Color(0xffedfff7),
+    500: Color(0xffe9fff5),
+    600: Color(0xffbaccc4),
+    700: Color(0xff8c9993),
+    800: Color(0xff5d6662),
+    900: Color(0xff2f3331)
   });
 
   static const MaterialColor aquamarine = MaterialColor(0xff82f5c3, {
-    50: Color.fromRGBO(130, 245, 195, .1),
-    100: Color.fromRGBO(130, 245, 195, .2),
-    200: Color.fromRGBO(130, 245, 195, .3),
-    300: Color.fromRGBO(130, 245, 195, .4),
-    400: Color.fromRGBO(130, 245, 195, .5),
-    500: Color.fromRGBO(130, 245, 195, .6),
-    600: Color.fromRGBO(130, 245, 195, .7),
-    700: Color.fromRGBO(130, 245, 195, .8),
-    800: Color.fromRGBO(130, 245, 195, .9),
-    900: Color.fromRGBO(130, 245, 195, 1),
+    50: Color(0xfff3fef9),
+    100: Color(0xffe6fdf3),
+    200: Color(0xffcdfbe7),
+    300: Color(0xffb4f9db),
+    400: Color(0xff9bf7cf),
+    500: Color(0xff82f5c3),
+    600: Color(0xff68c49c),
+    700: Color(0xff4e9375),
+    800: Color(0xff34624e),
+    900: Color(0xff1a3127)
   });
 
   static const MaterialColor ultramarine = MaterialColor(0xff441adb, {
-    50: Color.fromRGBO(68, 26, 219, .1),
-    100: Color.fromRGBO(68, 26, 219, .2),
-    200: Color.fromRGBO(68, 26, 219, .3),
-    300: Color.fromRGBO(68, 26, 219, .4),
-    400: Color.fromRGBO(68, 26, 219, .5),
-    500: Color.fromRGBO(68, 26, 219, .6),
-    600: Color.fromRGBO(68, 26, 219, .7),
-    700: Color.fromRGBO(68, 26, 219, .8),
-    800: Color.fromRGBO(68, 26, 219, .9),
-    900: Color.fromRGBO(68, 26, 219, 1),
+    50: Color(0xffece8fb),
+    100: Color(0xffdad1f8),
+    200: Color(0xffb4a3f1),
+    300: Color(0xff8f76e9),
+    400: Color(0xff6948e2),
+    500: Color(0xff441adb),
+    600: Color(0xff3615af),
+    700: Color(0xff291083),
+    800: Color(0xff1b0a58),
+    900: Color(0xff0e052c)
   });
 
   static const MaterialColor violet = MaterialColor(0xff8038dd, {
-    50: Color.fromRGBO(128, 56, 221, .1),
-    100: Color.fromRGBO(128, 56, 221, .2),
-    200: Color.fromRGBO(128, 56, 221, .3),
-    300: Color.fromRGBO(128, 56, 221, .4),
-    400: Color.fromRGBO(128, 56, 221, .5),
-    500: Color.fromRGBO(128, 56, 221, .6),
-    600: Color.fromRGBO(128, 56, 221, .7),
-    700: Color.fromRGBO(128, 56, 221, .8),
-    800: Color.fromRGBO(128, 56, 221, .9),
-    900: Color.fromRGBO(128, 56, 221, 1),
+    50: Color(0xfff2ebfc),
+    100: Color(0xffe6d7f8),
+    200: Color(0xffccaff1),
+    300: Color(0xffb388eb),
+    400: Color(0xff9960e4),
+    500: Color(0xff8038dd),
+    600: Color(0xff662db1),
+    700: Color(0xff4d2285),
+    800: Color(0xff331658),
+    900: Color(0xff1a0b2c)
   });
 
   static const MaterialColor white = MaterialColor(0xffffffff, {
-    50: Color.fromRGBO(255, 255, 255, .1),
+    50: Color.fromRGBO(255, 255, 255, .05),
     100: Color.fromRGBO(255, 255, 255, .2),
-    200: Color.fromRGBO(255, 255, 255, .3),
-    300: Color.fromRGBO(255, 255, 255, .4),
-    400: Color.fromRGBO(255, 255, 255, .5),
-    500: Color.fromRGBO(255, 255, 255, .6),
-    600: Color.fromRGBO(255, 255, 255, .7),
-    700: Color.fromRGBO(255, 255, 255, .8),
-    800: Color.fromRGBO(255, 255, 255, .9),
-    900: Color.fromRGBO(255, 255, 255, 1),
+    200: Color.fromRGBO(255, 255, 255, .4),
+    300: Color.fromRGBO(255, 255, 255, .6),
+    400: Color.fromRGBO(255, 255, 255, .8),
+    500: Color.fromRGBO(255, 255, 255, 1)
   });
 
-  static const MaterialColor success = aquamarine;
+  static const MaterialColor success = MaterialColor(0xff93f582, {
+    50: Color(0xfff4fef3),
+    100: Color(0xffe9fde6),
+    200: Color(0xffd4fbcd),
+    300: Color(0xffbef9b4),
+    400: Color(0xffa9f79b),
+    500: Color(0xff93f582),
+    600: Color(0xff76c468),
+    700: Color(0xff58934e),
+    800: Color(0xff3b6234),
+    900: Color(0xff1d311a)
+  });
 
   static const MaterialColor warning = MaterialColor(0xfff5f582, {
-    50: Color.fromRGBO(245, 245, 130, .1),
-    100: Color.fromRGBO(245, 245, 130, .2),
-    200: Color.fromRGBO(245, 245, 130, .3),
-    300: Color.fromRGBO(245, 245, 130, .4),
-    400: Color.fromRGBO(245, 245, 130, .5),
-    500: Color.fromRGBO(245, 245, 130, .6),
-    600: Color.fromRGBO(245, 245, 130, .7),
-    700: Color.fromRGBO(245, 245, 130, .8),
-    800: Color.fromRGBO(245, 245, 130, .9),
-    900: Color.fromRGBO(245, 245, 130, 1),
+    50: Color(0xfffefef3),
+    100: Color(0xfffdfde6),
+    200: Color(0xfffbfbcd),
+    300: Color(0xfff9f9b4),
+    400: Color(0xfff7f79b),
+    500: Color(0xfff5f582),
+    600: Color(0xffc4c468),
+    700: Color(0xff93934e),
+    800: Color(0xff626234),
+    900: Color(0xff31311a)
   });
 
   static const MaterialColor danger = MaterialColor(0xfff58282, {
-    50: Color.fromRGBO(245, 130, 130, .1),
-    100: Color.fromRGBO(245, 130, 130, .2),
-    200: Color.fromRGBO(245, 130, 130, .3),
-    300: Color.fromRGBO(245, 130, 130, .4),
-    400: Color.fromRGBO(245, 130, 130, .5),
-    500: Color.fromRGBO(245, 130, 130, .6),
-    600: Color.fromRGBO(245, 130, 130, .7),
-    700: Color.fromRGBO(245, 130, 130, .8),
-    800: Color.fromRGBO(245, 130, 130, .9),
-    900: Color.fromRGBO(245, 130, 130, 1),
+    50: Color(0xfffef3f3),
+    100: Color(0xfffde6e6),
+    200: Color(0xfffbcdcd),
+    300: Color(0xfff9b4b4),
+    400: Color(0xfff79b9b),
+    500: Color(0xfff58282),
+    600: Color(0xffc46868),
+    700: Color(0xff934e4e),
+    800: Color(0xff623434),
+    900: Color(0xff311a1a),
   });
 }
