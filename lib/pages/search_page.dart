aced9404ba026950528262b3d509e69194a615a5
Panitnun Suvannabun <Panitnun.6243@gmail.com>
2022-06-25 01:04:14 +0700

    Update Search page

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -1,9 +1,11 @@
 import 'package:ZeeU/utils/constants.dart';
+import 'package:ZeeU/utils/palette.dart';
 import 'package:ZeeU/widgets/search/specialty_card.dart';
 import 'package:flutter/material.dart';
 
 class SearchPage extends StatefulWidget {
-  const SearchPage({Key? key, required this.notifyRouteChange}) : super(key: key);
+  const SearchPage({Key? key, required this.notifyRouteChange})
+      : super(key: key);
 
   final Function(String, String) notifyRouteChange;
 
@@ -12,36 +14,53 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Colors.transparent,
         body: SingleChildScrollView(
           child: Padding(
-            padding: const EdgeInsets.all(15),
-            child: Column(
-              children: [
-                const Text('Search'),
-                const Text('Who are you seeking help from?'),
+              padding: const EdgeInsets.all(15),
+              child: Column(children: [
+                const SizedBox(
+                  height: 30,
+                ),
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: Text(
+                    'Search',
+                    style: Theme.of(context)
+                        .textTheme
+                        .headline1!
+                        .apply(color: Palette.jet),
+                  ),
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+                const Align(
+                  alignment: Alignment.topCenter,
+                  child: Text('Who are you seeking help from?'),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
                 Wrap(
-                  alignment: WrapAlignment.spaceAround,
-                  runAlignment: WrapAlignment.spaceAround,
-                  spacing: 20,
-                  runSpacing: 20,
-                  children: Constants.specialties
+                    alignment: WrapAlignment.spaceAround,
+                    runAlignment: WrapAlignment.spaceAround,
+                    spacing: 20,
+                    runSpacing: 20,
+                    children: Constants.specialties
                         .map((e) => SpecialtyCard(
                               specialty: e,
                               onTap: () {
-                                Navigator.of(context).pushNamed('/doctors', arguments: e);
+                                Navigator.of(context)
+                                    .pushNamed('/doctors', arguments: e);
                                 widget.notifyRouteChange('push', '/doctors');
                               },
                             ))
-                        .toList()
-                )
-              ]
-            )
-          ),
+                        .toList())
+              ])),
         ));
   }
 }
--- lib/widgets/search/specialty_card.dart
+++ lib/widgets/search/specialty_card.dart
@@ -1,13 +1,11 @@
 import 'package:ZeeU/utils/constants.dart';
 import 'package:ZeeU/utils/palette.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SpecialtyCard extends StatelessWidget {
-  const SpecialtyCard({
-    Key? key,
-    required this.specialty,
-    this.onTap
-  }) : super(key: key);
+  const SpecialtyCard({Key? key, required this.specialty, this.onTap})
+      : super(key: key);
 
   final String specialty;
   final Function()? onTap;
@@ -15,20 +13,54 @@ class SpecialtyCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: onTap,
-      child: Container(
-        width: 120,
-        height: 120,
-        decoration: const BoxDecoration(
-          color: Palette.white
-        ),
-        child: Column(
-          children: [
-            Text(specialty),
-            Image(image: AssetImage(Constants.specialtiesImg[specialty]!))
-          ],
-        )
-      )
-    );
+        onTap: onTap,
+        child: Container(
+            width: 120,
+            height: 120,
+            decoration: BoxDecoration(
+              color: const Color.fromRGBO(255, 255, 255, 1),
+              shape: BoxShape.circle,
+              border: Border.all(
+                color: const Color.fromRGBO(255, 255, 255, 1),
+                width: 0.5,
+              ),
+              boxShadow: const [
+                BoxShadow(
+                    color: Color.fromRGBO(53, 53, 53, 0.10000000149011612),
+                    offset: Offset(2, 3),
+                    blurRadius: 20)
+              ],
+              gradient: const LinearGradient(
+                  begin: Alignment(0.6023529171943665, 0.48941177129745483),
+                  end: Alignment(-0.48941177129745483, 0.6023529171943665),
+                  colors: [
+                    Color.fromRGBO(255, 255, 255, 0.5),
+                    Color.fromRGBO(255, 255, 255, 0.30000001192092896)
+                  ]),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image(
+                  image: AssetImage(
+                    Constants.specialtiesImg[specialty]!,
+                  ),
+                  width: 40,
+                  height: 40,
+                  fit: BoxFit.fitWidth,
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                Text(
+                  specialty,
+                  style: GoogleFonts.roboto(
+                    fontWeight: FontWeight.w500,
+                    color: Palette.jet,
+                    fontSize: 12,
+                  ),
+                ),
+              ],
+            )));
   }
 }
