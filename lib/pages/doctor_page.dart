892c5a6a79880dc8fc6ba3fd7a5450916831727b
Panitnun Suvannabun <Panitnun.6243@gmail.com>
2022-06-25 19:35:56 +0700

    Update Search result page and Doctor card

--- lib/pages/doctor_page.dart
+++ lib/pages/doctor_page.dart
@@ -10,16 +10,17 @@ import 'package:ZeeU/widgets/cloud_image.dart';
 import 'package:ZeeU/widgets/search/doctor_card.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 
 class DoctorPage extends StatefulWidget {
-  const DoctorPage({
-    Key? key,
-    required this.specialty,
-    required this.notifyRouteChange,
-    required this.changeTab
-  }) : super(key: key);
+  const DoctorPage(
+      {Key? key,
+      required this.specialty,
+      required this.notifyRouteChange,
+      required this.changeTab})
+      : super(key: key);
 
   final String specialty;
   final Function(String, String) notifyRouteChange;
@@ -31,116 +32,111 @@ class DoctorPage extends StatefulWidget {
 
 class _DoctorPageState extends State<DoctorPage> {
   final userRef = FirebaseFirestore.instance
-                .collection('users')
-                .withConverter<AppUser>(
-                    fromFirestore: (snapshots, _) => AppUser.fromJson(snapshots.data()!),
-                    toFirestore: (usr, _) => usr.toJson());
+      .collection('users')
+      .withConverter<AppUser>(
+          fromFirestore: (snapshots, _) => AppUser.fromJson(snapshots.data()!),
+          toFirestore: (usr, _) => usr.toJson());
 
-  Future<void> createChat({
-    required BuildContext ctx,
-    required String doctorUid,
-    required String patientUid
-  }) async {
+  Future<void> createChat(
+      {required BuildContext ctx,
+      required String doctorUid,
+      required String patientUid}) async {
     final confirm = await showDialog<bool>(
-      context: context,
-      useRootNavigator: false,
-      builder: (_) => AlertDialog(
-        title: const Text('Consult with this doctor?'),
-        actions: [
-          TextButton(
-            onPressed: () => Navigator.of(context).pop(false),
-            child: const Text('No')
-          ),
-          TextButton(
-            onPressed: () => Navigator.of(context).pop(true),
-            child: const Text('Yes')
-          )
-        ]
-      )
-    );
+        context: context,
+        useRootNavigator: false,
+        builder: (_) => AlertDialog(
+                title: const Text('Consult with this doctor?'),
+                actions: [
+                  TextButton(
+                      onPressed: () => Navigator.of(context).pop(false),
+                      child: const Text('No')),
+                  TextButton(
+                      onPressed: () => Navigator.of(context).pop(true),
+                      child: const Text('Yes'))
+                ]));
 
-    if(!(confirm ?? true)) return;
+    if (!(confirm ?? true)) return;
 
-    await FirebaseFirestore.instance
-      .collection('chats')
-      .add({
-        'doctor': doctorUid,
-        'patient': patientUid,
-        'latest_message_text': '',
-        'latest_message_time': Timestamp.now(),
-        'latest_message_seen_doctor': false,
-        'latest_message_seen_patient': false
-      });
-    
+    await FirebaseFirestore.instance.collection('chats').add({
+      'doctor': doctorUid,
+      'patient': patientUid,
+      'latest_message_text': '',
+      'latest_message_time': Timestamp.now(),
+      'latest_message_seen_doctor': false,
+      'latest_message_seen_patient': false
+    });
+
     widget.changeTab(TabItem.chats);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<UserState>(builder: (context, user, child) => 
-      WillPopScope(
-        onWillPop: () async {
-          widget.notifyRouteChange('pop', '/doctors');
-          return true;
-        },
-        child: SafeArea(
-          child: Scaffold(
-              backgroundColor: Colors.transparent,
-              appBar: AppBar(
-                  backgroundColor: Colors.transparent,
-                  foregroundColor: Palette.jet,
-                  title: Text(widget.specialty),
-                  elevation: 0),
-              body: FutureBuilder<QuerySnapshot<AppUser>>(
-                future: userRef
-                    .where('user_type', isEqualTo: 'doctor')
-                    .where('specialty', isEqualTo: widget.specialty)
-                    .get(),
-                builder: (context, snapshot) {
-                  if (snapshot.hasError) {
-                    return Center(
-                      child: Text(snapshot.error.toString()),
-                    );
-                  }
+    return Consumer<UserState>(
+        builder: (context, user, child) => WillPopScope(
+            onWillPop: () async {
+              widget.notifyRouteChange('pop', '/doctors');
+              return true;
+            },
+            child: SafeArea(
+                child: Scaffold(
+                    backgroundColor: Colors.transparent,
+                    appBar: AppBar(
+                        backgroundColor: Colors.transparent,
+                        foregroundColor: Palette.jet,
+                        title: Text(
+                          widget.specialty,
+                          style: GoogleFonts.roboto(
+                            fontWeight: FontWeight.w700,
+                            color: Palette.jet,
+                            fontSize: 18,
+                          ),
+                        ),
+                        elevation: 0),
+                    body: FutureBuilder<QuerySnapshot<AppUser>>(
+                      future: userRef
+                          .where('user_type', isEqualTo: 'doctor')
+                          .where('specialty', isEqualTo: widget.specialty)
+                          .get(),
+                      builder: (context, snapshot) {
+                        if (snapshot.hasError) {
+                          return Center(
+                            child: Text(snapshot.error.toString()),
+                          );
+                        }
 
-                  if (!snapshot.hasData) {
-                    return const Center(
-                        child: CircularProgressIndicator());
-                  }
+                        if (!snapshot.hasData) {
+                          return const Center(
+                              child: CircularProgressIndicator());
+                        }
 
-                  final data = snapshot.requireData.docs;
-                  final doctors = data.map((e) {
-                    final doc = e.data();
-                    doc.uid = e.id;
-                    return doc;
-                  });
+                        final data = snapshot.requireData.docs;
+                        final doctors = data.map((e) {
+                          final doc = e.data();
+                          doc.uid = e.id;
+                          return doc;
+                        });
 
-                  if (doctors.isEmpty) {
-                    return const Center(
-                      child: Text('Empty')
-                    );
-                  }
+                        if (doctors.isEmpty) {
+                          return const Center(child: Text('Empty'));
+                        }
 
-                  return Column(
-                    children: doctors.map((e) => DoctorCard(
-                        image: e.img!,
-                        name: '${e.firstName} ${e.lastName}',
-                        specialty: e.specialty!,
-                        institute: e.institute!,
-                        contact: e.contact!,
-                        onTap: user.userType == 'patient'
+                        return Column(
+                            children: doctors
+                                .map((e) => DoctorCard(
+                                      image: e.img!,
+                                      name: '${e.firstName} ${e.lastName}',
+                                      specialty: e.specialty!,
+                                      institute: e.institute!,
+                                      contact: e.contact!,
+                                      onTap: user.userType == 'patient'
                                           ? (ctx) => createChat(
                                               ctx: ctx,
                                               doctorUid: e.uid!,
                                               patientUid: user.uid!)
                                           : null,
-                      )).toList()
-                  );
-                },
-              )
-          )
-        )
-      )
-    );
+                                    ))
+                                .toList());
+                      },
+                    )))));
   }
 }
--- lib/widgets/search/doctor_card.dart
+++ lib/widgets/search/doctor_card.dart
@@ -1,17 +1,18 @@
 import 'package:ZeeU/utils/palette.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class DoctorCard extends StatelessWidget {
-  const DoctorCard({
-    Key? key,
-    required this.image,
-    required this.name,
-    required this.specialty,
-    required this.institute,
-    required this.contact,
-    this.onTap
-  }) : super(key: key);
+  const DoctorCard(
+      {Key? key,
+      required this.image,
+      required this.name,
+      required this.specialty,
+      required this.institute,
+      required this.contact,
+      this.onTap})
+      : super(key: key);
 
   final String image;
   final String name;
@@ -29,45 +30,109 @@ class DoctorCard extends StatelessWidget {
         child: Container(
             width: double.infinity,
             height: 92,
-            decoration: BoxDecoration(color: Palette.white),
+            decoration: BoxDecoration(
+              color: Palette.white,
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: const [
+                BoxShadow(
+                    color: Color.fromRGBO(53, 53, 53, 0.10000000149011612),
+                    offset: Offset(2, 3),
+                    blurRadius: 20),
+              ],
+              border: Border.all(
+                color: const Color.fromRGBO(255, 255, 255, 1),
+                width: 1,
+              ),
+            ),
             child: Row(children: [
               Container(
                 width: 7,
-                color: Palette.aquamarine,
+                decoration: const BoxDecoration(
+                  color: Palette.aquamarine,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(10),
+                    bottomLeft: Radius.circular(10),
+                  ),
+                ),
               ),
               const SizedBox(width: 15),
               Expanded(
-                child: Row(children: [
-                  Container(
-                    width: 60,
-                    height: 60,
-                    clipBehavior: Clip.hardEdge,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(15),
-                    ),
-                    child: FutureBuilder(
-                        future: FirebaseStorage.instance
-                            .refFromURL(image)
-                            .getDownloadURL(),
-                        builder: (context, AsyncSnapshot<String> snapshot) {
-                          if (snapshot.hasData && snapshot.data != null) {
-                            return Image(
-                              image: NetworkImage(snapshot.data!),
-                              fit: BoxFit.cover,
-                            );
-                          }
-                          return const CircularProgressIndicator();
-                        }),
-                  ),
-                  Column(
+                child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      Text(name),
-                      Text(specialty),
-                      Text(institute),
-                      Text(contact)
-                    ]
-                  ),
-                ]),
+                      Container(
+                        width: 60,
+                        height: 60,
+                        clipBehavior: Clip.hardEdge,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        child: FutureBuilder(
+                            future: FirebaseStorage.instance
+                                .refFromURL(image)
+                                .getDownloadURL(),
+                            builder: (context, AsyncSnapshot<String> snapshot) {
+                              if (snapshot.hasData && snapshot.data != null) {
+                                return Image(
+                                  image: NetworkImage(snapshot.data!),
+                                  fit: BoxFit.cover,
+                                );
+                              }
+                              return const CircularProgressIndicator();
+                            }),
+                      ),
+                      const SizedBox(width: 15),
+                      Expanded(
+                        child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                name,
+                                style: GoogleFonts.roboto(
+                                  fontWeight: FontWeight.w700,
+                                  color: Palette.jet,
+                                  fontSize: 14,
+                                ),
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                              const SizedBox(
+                                height: 5,
+                              ),
+                              Text(
+                                specialty,
+                                style: GoogleFonts.roboto(
+                                  fontWeight: FontWeight.w400,
+                                  color: Palette.jet,
+                                  fontSize: 12,
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 2.5,
+                              ),
+                              Text(
+                                institute,
+                                style: GoogleFonts.roboto(
+                                  fontWeight: FontWeight.w400,
+                                  color: Palette.jet,
+                                  fontSize: 12,
+                                ),
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                              const SizedBox(
+                                height: 5,
+                              ),
+                              Text(
+                                contact,
+                                style: GoogleFonts.roboto(
+                                  fontWeight: FontWeight.w400,
+                                  color: Palette.gray,
+                                  fontSize: 12,
+                                ),
+                              )
+                            ]),
+                      ),
+                    ]),
               )
             ])),
       ),
