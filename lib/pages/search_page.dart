406b38f92a30637d291dc05d1faa5c9ce75673a5
Panitnun Suvannabun <Panitnun.6243@gmail.com>
2022-06-27 02:51:11 +0700

    Customize Search header clipper

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -19,48 +19,124 @@ class _SearchPageState extends State<SearchPage> {
     return Scaffold(
         backgroundColor: Colors.transparent,
         body: SingleChildScrollView(
-          child: Padding(
-              padding: const EdgeInsets.all(15),
-              child: Column(children: [
-                const SizedBox(
-                  height: 30,
-                ),
-                Align(
-                  alignment: Alignment.topLeft,
-                  child: Text(
-                    'Search',
-                    style: Theme.of(context)
-                        .textTheme
-                        .headline1!
-                        .apply(color: Palette.jet),
+          child: Column(
+            children: [
+              Stack(
+                children: [
+                  ClipPath(
+                    clipper: GreenClipper(),
+                    child: Container(
+                      color: Palette.honeydew,
+                      height: 135,
+                    ),
                   ),
-                ),
-                const SizedBox(
-                  height: 50,
-                ),
-                const Align(
-                  alignment: Alignment.topCenter,
-                  child: Text('Who are you seeking help from?'),
-                ),
-                const SizedBox(
-                  height: 30,
-                ),
-                Wrap(
-                    alignment: WrapAlignment.spaceAround,
-                    runAlignment: WrapAlignment.spaceAround,
-                    spacing: 20,
-                    runSpacing: 20,
-                    children: Constants.specialties
-                        .map((e) => SpecialtyCard(
-                              specialty: e,
-                              onTap: () {
-                                Navigator.of(context)
-                                    .pushNamed('/doctors', arguments: e);
-                                widget.notifyRouteChange('push', '/doctors');
-                              },
-                            ))
-                        .toList())
-              ])),
+                  ClipPath(
+                    clipper: WhiteClipper(),
+                    child: Container(
+                      color: Palette.white,
+                      height: 130,
+                      child: Row(
+                        children: [
+                          const SizedBox(
+                            width: 15,
+                          ),
+                          Text(
+                            'Search',
+                            style: Theme.of(context)
+                                .textTheme
+                                .headline1!
+                                .apply(color: Palette.jet),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Padding(
+                  padding: const EdgeInsets.all(15),
+                  child: Column(children: [
+                    const Align(
+                      alignment: Alignment.topCenter,
+                      child: Text('Who are you seeking help from?'),
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    Wrap(
+                        alignment: WrapAlignment.spaceAround,
+                        runAlignment: WrapAlignment.spaceAround,
+                        spacing: 20,
+                        runSpacing: 20,
+                        children: Constants.specialties
+                            .map((e) => SpecialtyCard(
+                                  specialty: e,
+                                  onTap: () {
+                                    Navigator.of(context)
+                                        .pushNamed('/doctors', arguments: e);
+                                    widget.notifyRouteChange(
+                                        'push', '/doctors');
+                                  },
+                                ))
+                            .toList())
+                  ])),
+            ],
+          ),
         ));
+  }
+}
+
+class WhiteClipper extends CustomClipper<Path> {
+  double pi = 3.14;
+  @override
+  Path getClip(Size size) {
+    final roundingHeight = size.height * 3 / 5;
+
+    final filledRectangle =
+        Rect.fromLTRB(0, 0, size.width, size.height - roundingHeight);
+
+    final roundingRectangle = Rect.fromLTRB(
+        -70, size.height - roundingHeight * 2, size.width + 70, size.height);
+
+    final path = Path();
+    path.addRect(filledRectangle);
+    path.arcTo(roundingRectangle, pi, -pi, true);
+    path.close();
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    return true;
+  }
+}
+
+class GreenClipper extends CustomClipper<Path> {
+  double pi = 3.14159265359;
+  @override
+  Path getClip(Size size) {
+    final roundingHeight = size.height * 3 / 5;
+
+    final filledRectangle =
+        Rect.fromLTRB(0, 0, size.width, size.height - roundingHeight);
+
+    final roundingRectangle = Rect.fromLTRB(
+        -70, size.height - roundingHeight * 2, size.width + 85, size.height);
+
+    final path = Path();
+    path.addRect(filledRectangle);
+    path.arcTo(roundingRectangle, pi, -pi, true);
+    path.close();
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    return true;
   }
 }
