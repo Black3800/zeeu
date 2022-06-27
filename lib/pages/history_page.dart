b32d564b184d02db4d0b24cb247b2f6946a1b607
Aerith <chutirat.earth76@mail.kmutt.ac.th>
2022-06-28 00:06:26 +0700

    style card

--- lib/pages/history_page.dart
+++ lib/pages/history_page.dart
@@ -10,111 +10,116 @@ import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 
 class HistoryPage extends StatelessWidget {
-  HistoryPage({ Key? key, required this.notifyRouteChange }) : super(key: key);
+  HistoryPage({Key? key, required this.notifyRouteChange}) : super(key: key);
 
   final Function(String, String) notifyRouteChange;
   final appointmentRef = FirebaseFirestore.instance
       .collection('appointments')
       .withConverter<Appointment>(
-        fromFirestore: (snapshots, _) => Appointment.fromJson(snapshots.data()!),
-        toFirestore: (appointment, _) => appointment.toJson()
-      );
+          fromFirestore: (snapshots, _) =>
+              Appointment.fromJson(snapshots.data()!),
+          toFirestore: (appointment, _) => appointment.toJson());
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<UserState>(builder: (context, user, child) => 
-      WillPopScope(
-        onWillPop: () async {
-          notifyRouteChange('pop', '/history');
-          return true;
-        },
-        child: Scaffold(
-          backgroundColor: Colors.transparent,
-          appBar: AppBar(
-            title: const Text('History'),
-            centerTitle: true,
-            backgroundColor: Palette.white,
-            foregroundColor: Palette.jet,
-          ),
-          body: SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.all(15),
-              child:
-                FutureBuilder<QuerySnapshot<Appointment>>(
-                  future: appointmentRef
-                            .where(user.userType!, isEqualTo: user.uid)
-                            .where('start', isLessThan: Timestamp.now())
-                            .orderBy('start')
-                            .get(),
-                  builder: (context, snapshot) {
-                    if (snapshot.hasError) {
-                      return Center(
-                        child: Text(snapshot.error.toString()),
-                      );
-                    }
+    return Consumer<UserState>(
+        builder: (context, user, child) => WillPopScope(
+              onWillPop: () async {
+                notifyRouteChange('pop', '/history');
+                return true;
+              },
+              child: Scaffold(
+                  backgroundColor: Colors.transparent,
+                  appBar: AppBar(
+                    elevation: 0,
+                    title: const Text('History'),
+                    centerTitle: true,
+                    backgroundColor: Palette.white,
+                    foregroundColor: Palette.jet,
+                  ),
+                  body: SingleChildScrollView(
+                    child: Padding(
+                        padding: const EdgeInsets.all(15),
+                        child: FutureBuilder<QuerySnapshot<Appointment>>(
+                            future: appointmentRef
+                                .where(user.userType!, isEqualTo: user.uid)
+                                .where('start', isLessThan: Timestamp.now())
+                                .orderBy('start')
+                                .get(),
+                            builder: (context, snapshot) {
+                              if (snapshot.hasError) {
+                                return Center(
+                                  child: Text(snapshot.error.toString()),
+                                );
+                              }
 
-                    if (!snapshot.hasData) {
-                      return const Center(child: CircularProgressIndicator());
-                    }
+                              if (!snapshot.hasData) {
+                                return const Center(
+                                    child: CircularProgressIndicator());
+                              }
 
-                    final docs = snapshot.requireData.docs.reversed.toList();
-                    final appointments = [];
-                    var previousAppointmentDate = '';
-                    for (var i = 0; i < docs.length; i++) {
-                      final a = docs[i].data();
-                      final date = DateFormat('yMMMMd').format(a.start);
-                      if (date != previousAppointmentDate) {
-                        appointments.add(AppointmentDivider(text: date));
-                      }
-                      appointments.add(a);
-                      previousAppointmentDate = date;
-                    }
+                              final docs =
+                                  snapshot.requireData.docs.reversed.toList();
+                              final appointments = [];
+                              var previousAppointmentDate = '';
+                              for (var i = 0; i < docs.length; i++) {
+                                final a = docs[i].data();
+                                final date =
+                                    DateFormat('yMMMMd').format(a.start);
+                                if (date != previousAppointmentDate) {
+                                  appointments
+                                      .add(AppointmentDivider(text: date));
+                                }
+                                appointments.add(a);
+                                previousAppointmentDate = date;
+                              }
 
-                    if (appointments.isEmpty) {
-                      return const Center(
-                        child: Text('Nothing')
-                      );
-                    }
+                              if (appointments.isEmpty) {
+                                return const Center(child: Text('Nothing'));
+                              }
 
-                    return Column(children:
-                              appointments.map<Widget>((a) => a is AppointmentDivider ? a :
-                                FutureBuilder<DocumentSnapshot>(
-                                  future: FirebaseFirestore.instance
-                                      .collection('users')
-                                      .doc(user.userType == 'patient'
-                                            ? a.doctor.uid
-                                            : a.patient.uid)
-                                      .get(),
-                                  builder: (context, snapshot) {
-                                    if (snapshot.hasError) {
-                                      return Center(
-                                        child: Text(snapshot.error.toString()),
-                                      );
-                                    }
+                              return Column(
+                                  children: appointments
+                                      .map<Widget>((a) => a
+                                              is AppointmentDivider
+                                          ? a
+                                          : FutureBuilder<DocumentSnapshot>(
+                                              future: FirebaseFirestore.instance
+                                                  .collection('users')
+                                                  .doc(
+                                                      user.userType == 'patient'
+                                                          ? a.doctor.uid
+                                                          : a.patient.uid)
+                                                  .get(),
+                                              builder: (context, snapshot) {
+                                                if (snapshot.hasError) {
+                                                  return Center(
+                                                    child: Text(snapshot.error
+                                                        .toString()),
+                                                  );
+                                                }
 
-                                    if (!snapshot.hasData) {
-                                      return const Center(
-                                        child: CircularProgressIndicator()
-                                      );
-                                    }
+                                                if (!snapshot.hasData) {
+                                                  return const Center(
+                                                      child:
+                                                          CircularProgressIndicator());
+                                                }
 
-                                    final data = snapshot.requireData.data() as Map;
-                                    final user = AppUser.fromJson(data);
-                                    return AppointmentCard(
-                                        image: user.img!,
-                                        name: '${user.firstName} ${user.lastName}',
-                                        institute: user.institute ?? 'Patient'
-                                    );
-                                  }
-                                )
-                            ).toList()
-                          );
-                    }
-              )
-            ),
-          )
-        ),
-      )
-    );
+                                                final data =
+                                                    snapshot.requireData.data()
+                                                        as Map;
+                                                final user =
+                                                    AppUser.fromJson(data);
+                                                return AppointmentCard(
+                                                    image: user.img!,
+                                                    name:
+                                                        '${user.firstName} ${user.lastName}',
+                                                    institute: user.institute ??
+                                                        'Patient');
+                                              }))
+                                      .toList());
+                            })),
+                  )),
+            ));
   }
 }
--- lib/widgets/home/appointment_card.dart
+++ lib/widgets/home/appointment_card.dart
@@ -1,16 +1,19 @@
+import 'dart:ui';
+
 import 'package:ZeeU/utils/palette.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class AppointmentCard extends StatelessWidget {
-  const AppointmentCard({
-    Key? key,
-    required this.image,
-    required this.name,
-    required this.institute,
-    this.startTime,
-    this.endTime
-  }) : super(key: key);
+  const AppointmentCard(
+      {Key? key,
+      required this.image,
+      required this.name,
+      required this.institute,
+      this.startTime,
+      this.endTime})
+      : super(key: key);
 
   final String image;
   final String name;
@@ -23,30 +26,46 @@ class AppointmentCard extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 30),
       child: Container(
-        width: double.infinity,
-        height: 92,
-        decoration: BoxDecoration(
-          color: Palette.white
-        ),
-        child: Row(
-          children: [
+          width: double.infinity,
+          height: 92,
+          decoration: BoxDecoration(
+              color: Palette.white,
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: const [
+                BoxShadow(
+                  color: Color.fromRGBO(53, 53, 53, 0.10000000149011612),
+                  offset: Offset(2, 3),
+                  blurRadius: 20,
+                ),
+              ],
+              border: Border.all(
+                color: const Color.fromRGBO(225, 225, 225, 1),
+                width: 1,
+              )),
+          child: Row(children: [
             Container(
               width: 7,
-              color: Palette.aquamarine,
+              decoration: const BoxDecoration(
+                  color: Palette.aquamarine,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(10),
+                    bottomLeft: Radius.circular(10),
+                  )),
             ),
             const SizedBox(width: 15),
             Expanded(
-              child: Row(
-                children: [
-                  Container(
-                    width: 60,
-                    height: 60,
-                    clipBehavior: Clip.hardEdge,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(15),
-                    ),
-                    child: FutureBuilder(
-                      future: FirebaseStorage.instance.refFromURL(image).getDownloadURL(),
+              child: Row(children: [
+                Container(
+                  width: 60,
+                  height: 60,
+                  clipBehavior: Clip.hardEdge,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: FutureBuilder(
+                      future: FirebaseStorage.instance
+                          .refFromURL(image)
+                          .getDownloadURL(),
                       builder: (context, AsyncSnapshot<String> snapshot) {
                         if (snapshot.hasData && snapshot.data != null) {
                           return Image(
@@ -55,28 +74,44 @@ class AppointmentCard extends StatelessWidget {
                           );
                         }
                         return const CircularProgressIndicator();
-                      }
+                      }),
+                ),
+                const SizedBox(width: 15),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      name,
+                      style: GoogleFonts.roboto(
+                        fontWeight: FontWeight.w700,
+                        fontSize: 14,
+                        color: Palette.jet,
+                      ),
+                      overflow: TextOverflow.ellipsis,
                     ),
-                  ),
+                    const SizedBox(
+                      height: 4,
+                    ),
+                    Text(
+                      institute,
+                      style: GoogleFonts.roboto(
+                        fontWeight: FontWeight.w400,
+                        color: Palette.gray,
+                        fontSize: 12,
+                      ),
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ],
+                ),
+                if (startTime != null)
                   Column(
-                    children: [
-                      Text(name),
-                      Text(institute)
-                    ]
-                  ),
-                  if (startTime != null)
-                    Column(
-                      children: [
-                        Text(startTime!),
-                        Text(endTime!)
-                      ]
-                    )
-                ]
-              ),
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [Text(startTime!), Text(endTime!)])
+              ]),
             )
-          ]
-        )
-      ),
+          ])),
     );
   }
 }
