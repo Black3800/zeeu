342109dfa295b12ecf8ed5a726ba330c736377c8
Panitnun Suvannabun <Panitnun.6243@gmail.com>
2022-06-29 04:10:15 +0700

    Update Chat lists page

--- lib/widgets/chats/chat_card.dart
+++ lib/widgets/chats/chat_card.dart
@@ -1,17 +1,18 @@
 import 'package:ZeeU/utils/palette.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ChatCard extends StatelessWidget {
-  const ChatCard({
-    Key? key,
-    required this.image,
-    required this.name,
-    required this.text,
-    required this.time,
-    required this.seen,
-    this.onTap
-  }) : super(key: key);
+  const ChatCard(
+      {Key? key,
+      required this.image,
+      required this.name,
+      required this.text,
+      required this.time,
+      required this.seen,
+      this.onTap})
+      : super(key: key);
 
   final Function()? onTap;
   final String image, name, text, time;
@@ -22,50 +23,119 @@ class ChatCard extends StatelessWidget {
     return InkWell(
       onTap: onTap,
       child: Container(
+        margin: const EdgeInsets.symmetric(vertical: 0.2),
         height: 80,
         decoration: BoxDecoration(
-          border: Border.all(),
-          color: Palette.white
+          border: Border.all(
+            color: Palette.gray.shade200,
+            width: 0.5,
+          ),
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Palette.white.withOpacity(.5),
+              Palette.white.withOpacity(.3)
+            ],
+          ),
         ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            FutureBuilder(
-              future: FirebaseStorage.instance.refFromURL(image).getDownloadURL(),
-              builder: (context, AsyncSnapshot<String> snapshot) {
-                if (snapshot.hasData && snapshot.data != null) {
-                  return Image(
-                    image: NetworkImage(snapshot.data!),
-                    fit: BoxFit.cover,
-                  );
-                }
-                return const CircularProgressIndicator();
-              }
-            ),
-            Column(
-              children: [
-                Text(name),
-                Text(text)
-              ],
-            ),
-            Column(
-              children: [
-                Text(time),
-                if (!seen) disc()
-              ],
-            )
-          ],
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              FutureBuilder(
+                  future: FirebaseStorage.instance
+                      .refFromURL(image)
+                      .getDownloadURL(),
+                  builder: (context, AsyncSnapshot<String> snapshot) {
+                    if (snapshot.hasData && snapshot.data != null) {
+                      return Container(
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          boxShadow: [
+                            BoxShadow(
+                                color: Color.fromRGBO(53, 53, 53, 0.25),
+                                offset: Offset(1, 2),
+                                blurRadius: 10)
+                          ],
+                        ),
+                        child: CircleAvatar(
+                          radius: 28.0,
+                          backgroundImage: NetworkImage(snapshot.data!),
+                          backgroundColor: Colors.transparent,
+                        ),
+                      );
+                      //     Image(
+                      //   image: NetworkImage(snapshot.data!),
+                      //   fit: BoxFit.cover,
+                      // );
+                    }
+                    return const CircularProgressIndicator();
+                  }),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        name,
+                        overflow: TextOverflow.ellipsis,
+                        style: GoogleFonts.roboto(
+                          fontWeight: FontWeight.w700,
+                          color: Palette.jet,
+                          fontSize: 16,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 12,
+                      ),
+                      Text(
+                        text,
+                        overflow: TextOverflow.ellipsis,
+                        style: GoogleFonts.roboto(
+                          fontWeight: FontWeight.w400,
+                          color: Palette.jet,
+                          fontSize: 14,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    time,
+                    style: GoogleFonts.roboto(
+                      fontWeight: FontWeight.w400,
+                      color: Palette.gray,
+                      fontSize: 12,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  SizedBox(
+                    height: seen ? 10 : 0,
+                  ),
+                  if (!seen) disc()
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
   }
 
   Widget disc() => Container(
-                  width: 10,
-                  height: 10,
-                  decoration: const BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: Palette.aquamarine
-                  ),
-                );
+        width: 10,
+        height: 10,
+        decoration: const BoxDecoration(
+            shape: BoxShape.circle, color: Palette.aquamarine),
+      );
 }
--- lib/widgets/chats/search_bar.dart
+++ lib/widgets/chats/search_bar.dart
@@ -1,21 +1,55 @@
+import 'package:ZeeU/utils/palette.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SearchBar extends StatelessWidget {
-  const SearchBar({ Key? key, required this.onSubmit }) : super(key: key);
+  const SearchBar({Key? key, required this.onSubmit}) : super(key: key);
 
   final Function(String)? onSubmit;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.all(20),
-      child: TextFormField(
-        decoration: InputDecoration(
-          hintText: 'Search'
-        ),
-        onFieldSubmitted: onSubmit,
-        autofocus: false
-      )
-    );
+        padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+        child: TextFormField(
+            minLines: 1,
+            decoration: InputDecoration(
+              prefixIconConstraints:
+                  const BoxConstraints(minWidth: 50, maxHeight: 20),
+              prefixIcon: const Padding(
+                padding: EdgeInsets.all(0.0),
+                child: Icon(
+                  Icons.search_outlined,
+                  color: Palette.aquamarine,
+                ),
+              ),
+              isDense: true,
+              contentPadding:
+                  const EdgeInsets.symmetric(vertical: 10.0, horizontal: 22.0),
+              fillColor: Palette.honeydew,
+              filled: true,
+              hintText: 'Search',
+              hintStyle: GoogleFonts.roboto(
+                fontWeight: FontWeight.w600,
+                color: Palette.aquamarine.shade500,
+                fontSize: 15,
+              ),
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(15.0),
+                borderSide: const BorderSide(
+                  color: Palette.aquamarine,
+                  width: 0.7,
+                ),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(15.0),
+                borderSide: BorderSide(
+                  color: Palette.aquamarine.shade600,
+                  width: 1,
+                ),
+              ),
+            ),
+            onFieldSubmitted: onSubmit,
+            autofocus: false));
   }
 }
