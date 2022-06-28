764c20bd6f916c4cb51346fd27d8e0c47572c256
Panitnun Suvannabun <Panitnun.6243@gmail.com>
2022-06-28 21:23:21 +0700

    Fix appointment cart layout

--- lib/widgets/home/appointment_card.dart
+++ lib/widgets/home/appointment_card.dart
@@ -26,23 +26,24 @@ class AppointmentCard extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 30),
       child: Container(
-          width: double.infinity,
-          height: 92,
-          decoration: BoxDecoration(
-              color: Palette.white,
-              borderRadius: BorderRadius.circular(10),
-              boxShadow: const [
-                BoxShadow(
-                  color: Color.fromRGBO(53, 53, 53, 0.10000000149011612),
-                  offset: Offset(2, 3),
-                  blurRadius: 20,
-                ),
-              ],
-              border: Border.all(
-                color: const Color.fromRGBO(225, 225, 225, 1),
-                width: 1,
-              )),
-          child: Row(children: [
+        width: double.infinity,
+        height: 92,
+        decoration: BoxDecoration(
+            color: Palette.white,
+            borderRadius: BorderRadius.circular(10),
+            boxShadow: const [
+              BoxShadow(
+                color: Color.fromRGBO(53, 53, 53, 0.10000000149011612),
+                offset: Offset(2, 3),
+                blurRadius: 20,
+              ),
+            ],
+            border: Border.all(
+              color: const Color.fromRGBO(225, 225, 225, 1),
+              width: 1,
+            )),
+        child: Row(
+          children: [
             Container(
               width: 7,
               decoration: const BoxDecoration(
@@ -54,64 +55,80 @@ class AppointmentCard extends StatelessWidget {
             ),
             const SizedBox(width: 15),
             Expanded(
-              child: Row(children: [
-                Container(
-                  width: 60,
-                  height: 60,
-                  clipBehavior: Clip.hardEdge,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(15),
+              child: Row(
+                children: [
+                  Container(
+                    width: 60,
+                    height: 60,
+                    clipBehavior: Clip.hardEdge,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: FutureBuilder(
+                        future: FirebaseStorage.instance
+                            .refFromURL(image)
+                            .getDownloadURL(),
+                        builder: (context, AsyncSnapshot<String> snapshot) {
+                          if (snapshot.hasData && snapshot.data != null) {
+                            return Image(
+                              image: NetworkImage(snapshot.data!),
+                              fit: BoxFit.cover,
+                            );
+                          }
+                          return const CircularProgressIndicator();
+                        }),
                   ),
-                  child: FutureBuilder(
-                      future: FirebaseStorage.instance
-                          .refFromURL(image)
-                          .getDownloadURL(),
-                      builder: (context, AsyncSnapshot<String> snapshot) {
-                        if (snapshot.hasData && snapshot.data != null) {
-                          return Image(
-                            image: NetworkImage(snapshot.data!),
-                            fit: BoxFit.cover,
-                          );
-                        }
-                        return const CircularProgressIndicator();
-                      }),
-                ),
-                const SizedBox(width: 15),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      name,
-                      style: GoogleFonts.roboto(
-                        fontWeight: FontWeight.w700,
-                        fontSize: 14,
-                        color: Palette.jet,
-                      ),
-                      overflow: TextOverflow.ellipsis,
+                  const SizedBox(width: 15),
+                  Expanded(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              name,
+                              style: GoogleFonts.roboto(
+                                fontWeight: FontWeight.w700,
+                                fontSize: 14,
+                                color: Palette.jet,
+                              ),
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                            const SizedBox(
+                              height: 4,
+                            ),
+                            Text(
+                              institute,
+                              style: GoogleFonts.roboto(
+                                fontWeight: FontWeight.w400,
+                                color: Palette.gray,
+                                fontSize: 12,
+                              ),
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ],
+                        ),
+                        if (startTime != null)
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(startTime!),
+                              Text(endTime!),
+                            ],
+                          )
+                      ],
                     ),
-                    const SizedBox(
-                      height: 4,
-                    ),
-                    Text(
-                      institute,
-                      style: GoogleFonts.roboto(
-                        fontWeight: FontWeight.w400,
-                        color: Palette.gray,
-                        fontSize: 12,
-                      ),
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                  ],
-                ),
-                if (startTime != null)
-                  Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [Text(startTime!), Text(endTime!)])
-              ]),
-            )
-          ])),
+                  ),
+                  const SizedBox(width: 15),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
